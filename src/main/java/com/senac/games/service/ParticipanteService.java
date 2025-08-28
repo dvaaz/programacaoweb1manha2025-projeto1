8acdc283198afc32bd2dd7f4824268f2748e1749
dvaaz <darley.azevedo@gmail.com>
2025-08-28 12:12:29 -0300

    Commit AutoWired ModelMapper

--- src/main/java/com/senac/games/service/ParticipanteService.java
+++ src/main/java/com/senac/games/service/ParticipanteService.java
@@ -4,6 +4,8 @@ import com.senac.games.dto.request.ParticipanteDTORequest;
 import com.senac.games.dto.response.ParticipanteDTOResponse;
 import com.senac.games.entities.Participante;
 import com.senac.games.repository.ParticipanteRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 public class ParticipanteService {
 
     private final ParticipanteRepository participanteRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public ParticipanteService(ParticipanteRepository participanteRepository) {
         this.participanteRepository = participanteRepository;
@@ -25,23 +30,31 @@ public class ParticipanteService {
             return this.participanteRepository.findById(participanteId).orElse(null);
     }
 
-    public ParticipanteDTOResponse criarParticipante(ParticipanteDTORequest participanteDTO) {
-        Participante participante = new Participante();
-        participante.setNome(participanteDTO.getNome());
+    public ParticipanteDTOResponse criarParticipante(ParticipanteDTORequest participanteDTORequest) {
+        // Mapper recebe o DTO de um tipo e o transforma em Participante
+        Participante participante = modelMapper.map(participanteDTORequest, Participante.class);
+
+
+       /* participante.setNome(participanteDTO.getNome());
         participante.setEmail(participanteDTO.getEmail());
         participante.setEndereco(participanteDTO.getEndereco());
         participante.setStatus(participanteDTO.getStatus());
         participante.setIdentificacao(participanteDTO.getIndentificacao());
+        */
 
+        //Salva o Participante no repository utilizando o método .save
         Participante participanteSave = this.participanteRepository.save(participante);
+        ParticipanteDTOResponse participanteDTOResponse = modelMapper.map(participanteSave, ParticipanteDTOResponse.class);
 
-        ParticipanteDTOResponse participanteDTOResponse = new ParticipanteDTOResponse();
+        /* ParticipanteDTOResponse participanteDTOResponse = new ParticipanteDTOResponse();
         participanteDTOResponse.setId(participanteSave.getId());
         participanteDTOResponse.setNome(participanteSave.getNome());
         participanteDTOResponse.setEmail(participanteSave.getEmail());
         participanteDTOResponse.setEndereco(participanteSave.getEndereco());
         participanteDTOResponse.setIndentificacao(participanteSave.getIdentificacao());
         participanteDTOResponse.setStatus(participanteSave.getStatus());
+        */
+
 
         return participanteDTOResponse;
     }
