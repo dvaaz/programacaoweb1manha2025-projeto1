f4a7cccf6ec6f63f20f94cf8c5592c67c19f18c6
Darley Vieira Araujo de Azevedo <darley.azevedo@gmail.com>
2025-09-01 13:07:48 -0300

    Add @Repository annotation to ParticipanteRepository

--- src/main/java/com/senac/games/repository/ParticipanteRepository.java
+++ src/main/java/com/senac/games/repository/ParticipanteRepository.java
@@ -2,6 +2,8 @@ package com.senac.games.repository;
 
 import com.senac.games.entities.Participante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParticipanteRepository extends JpaRepository<Participante,Integer> {
 }
